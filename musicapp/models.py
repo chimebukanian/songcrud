e828090967fb9e02d562ad986ce0e6660832682c
chimebukanian <chimebukanyanwu@gmail.com>
2022-10-31 18:03:17 +0100

    strfunc

--- musicapp/models.py
+++ musicapp/models.py
@@ -6,12 +6,21 @@ class Artiste(models.Model):
     last_name=models.CharField(max_length=100)
     age=models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.first_name
+
+
 class Song(models.Model):
     title=models.CharField(max_length=100)
     date_released=models.DateField(auto_now_add=True, null=True)
     likes=models.IntegerField(null=True,)
     artise_id=models.ForeignKey(Artiste, verbose_name=_("artiste"), on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
 
 class Lyric(models.Model):
     content=models.CharField(max_lenggth=100)
     song_id=models.OneToOneField(Song, verbose_name=_("song"), on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
